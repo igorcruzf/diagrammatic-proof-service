7a73c7359ff54a873411a60e90f497b8aa51ac76
igorcruzf <igfigueiredo@uolinc.com>
2022-04-20 16:11:50 -0300

    first commit

---
+++ src/main/kotlin/com/br/uff/tcc/DiagrammaticProofServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.br.uff.tcc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiagrammaticProofServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiagrammaticProofServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/uff/tcc/DiagrammaticProofServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.uff.tcc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiagrammaticProofServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
